f7411facdaa745ebef8014a636497bedf780a5e6
Ielmahi <vodkailias@gmail.com>
2024-05-03 16:46:50 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,7 @@
-# install flask from pip3.
+# installs puppet-lint package
 
-# ensure that python present 
-package { 'python3.8':
-  ensure => present,
-}
-
-# ensure that pip present
-package { 'python3-pip':
-  ensure => present,
-}
-
-# Install Flask 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
-}
-
-# Install Werkzeug 
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+  name     => 'flask',
+  provider => 'pip3',
 }
